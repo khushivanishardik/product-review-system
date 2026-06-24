099a9d1d6c7eb54c3e605020776ccb944563521c
khushi kumari <khushikumari5155@gmail.com>
2026-06-24 15:25:47 +0530

    calling backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://product-review-system-ci40.onrender.com",
+  baseURL: "https://product-review-system-ci40.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
